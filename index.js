3a3bf1b4647f278e093a9604e21a53cb96020406
Luciana Bossi Cardenas <lucianafbossi@gmail.com>
2022-11-23 19:03:02 +0100

    dropdown
    dropdown (select category)

---
+++ index.js
@@ -0,0 +1,26 @@
+//getting select from html
+const selectCategory = document.getElementById('categories');
+
+//getting category from API
+async function getCategory() {
+    try {
+        const response = await fetch("https://opentdb.com/api_category.php");
+        const data = response.json();
+        return data;
+    } catch (err) {
+        console.log(err)
+    }    
+}
+
+//displaying categories
+async function displayCategories() {
+    const options = await getCategory();
+    console.log(options)
+    for(let i=0; i<options.trivia_categories.length; i++) {
+        const newOption = document.createElement('option');
+        newOption.value = options.trivia_categories[i].id;
+        newOption.innerText = options.trivia_categories[i].name;
+        selectCategory.appendChild(newOption);
+    }        
+};
+displayCategories();
